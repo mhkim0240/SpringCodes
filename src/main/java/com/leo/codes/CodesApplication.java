4e4989d1b5fbe35f3438905d0fc7ba33ab354ea8
AzureAD\mhkim <mhkim0720@stato.co.kr>
2024-07-05 15:00:40 +0900

    - REST API 송수신 post data - Swagger Api 1. 인텔리 제이 애플리케이션 모드 실행 2. 인텔리 제이 스마트 톰캣 모드 실행.      a. jar, war 빌드 차이.      b. name-version.war vs ROOT.war - context path. 3. 스프링과 스프링 부트일때 web.xml 의 용도 대체 방법. 4. 스마트 톰캣이면서 war 일때 서블릿 컨테이너 초기화 방법.

--- src/main/java/com/leo/codes/CodesApplication.java
+++ src/main/java/com/leo/codes/CodesApplication.java
@@ -9,16 +9,24 @@ import org.joda.time.format.DateTimeFormatter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
 
 
 @SpringBootApplication
-public class CodesApplication {
+public class CodesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodesApplication.class, args);
 		//test();
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CodesApplication.class);
 	}
 
 
@@ -42,3 +50,11 @@ public class CodesApplication {
 
 
 }
+/*
+-DAPP_HOME=D:\git-my\SpringCodes
+-DAPP_CONF=D:\git-my\SpringCodes\conf
+-DdatabaseConfigPath=D:\git-my\SpringCodes\conf
+-Dlogback.configurationFile=D:\git-my\SpringCodes\conf\logback.xml
+-Dfile.encoding=euc-kr
+-Dconsole.encoding=euc-kr
+ */
