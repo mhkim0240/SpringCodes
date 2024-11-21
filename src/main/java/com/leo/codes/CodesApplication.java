62e0daa044d20ff7c126c2bb2029385063dedb18
AzureAD\mhkim <mhkim0720@stato.co.kr>
2024-11-21 16:43:22 +0900

    update

--- src/main/java/com/leo/codes/CodesApplication.java
+++ src/main/java/com/leo/codes/CodesApplication.java
@@ -7,6 +7,11 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 */
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -18,11 +23,25 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class CodesApplication extends SpringBootServletInitializer {
 
+	//private static final Logger log2= LoggerFactory.getLogger(CodesApplication.class);
+
+	private final Log log = LogFactory.getLog(SpringApplicationBuilder.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CodesApplication.class, args);
+
+
+
 		//test();
 	}
-
+/*
+-DAPP_HOME=D:\git-spam\ems\ems\
+-DAPP_CONF=D:\git-spam\ems\ems\conf
+-DdatabaseConfigPath=D:\git-spam\ems\ems\conf
+-Dlogback.configurationFile=D:\git-spam\ems\ems\conf\logback.xml
+-Dfile.encoding=euc-kr
+-Dconsole.encoding=euc-kr
+* */
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -37,7 +56,17 @@ public class CodesApplication extends SpringBootServletInitializer {
 
 	void test()
 	{
-		System.out.println("test");
+		log.trace("TRACE!!");
+		log.debug("DEBUG!!");
+		log.info("INFO!!");
+		log.warn("WARN!!");
+		log.error("ERROR!!");
+
+		//int i = 0;
+		//log.info("aaa : {}", i);
+
+		//log.debug("aaa2 : {}", i);
+		//System.out.println("test");
 /*
 		DateTimeZone seoul = DateTimeZone.forID("Asia/Seoul");
 		DateTime dateTime = new DateTime(seoul);
